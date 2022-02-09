1b89af4ff01290a316a53e2e2cfb649bf2b3d704
jjmachan <jamesjithin97@gmail.com>
2022-02-09 16:57:42 +0530

    more comments

--- aws-lambda/1_build_bento/bento.py
+++ aws-lambda/1_build_bento/bento.py
@@ -1,12 +1,17 @@
+from typing import Dict
+
 import bentoml
 from bentoml.io import JSON, Text
 
+# create a runner with the sentiment_clf model
 sentiment_clf_runner = bentoml.transformers.load_runner(
     "sentiment_clf:latest", tasks="sentiment-analysis", lm_head="classifier"
 )
 
+# create a new bentoml service and the sentiment_clf_runner to it
 svc = bentoml.Service("sentiment_analysis", runners=[sentiment_clf_runner])
 
+# dict to convert Lable from model -> Human readable label
 keys = {
     "LABEL_0": "NEGATIVE",
     "LABEL_1": "NEUTRAL",
@@ -14,10 +19,21 @@ keys = {
 }
 
 
+# define the /predict endpoint that is used for inference
 @svc.api(input=Text(), output=JSON())
-def predict(input_text):
+def predict(input_text: str) -> Dict:
+    """
+    /predict endpoint
+
+    This endpoint takes a Text input, runs the inference with the runner
+    and returns the result as JSON.
+    """
+    # run inference with the input_text that is received to the endpoint
     result = sentiment_clf_runner.run(input_text)
 
+    # post-processing
     if result["label"] in keys:
         result["label"] = keys[result["label"]]
+
+    # return the result as response
     return result
--- aws-lambda/1_build_bento/train.py
+++ aws-lambda/1_build_bento/train.py
@@ -4,11 +4,13 @@ import bentoml
 
 # PLEASE UNCOMMENT THE MODEL YOU WANT TO USE
 # MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
-# MODEL = "dhpollack/distilbert-dummy-sentiment"
 # MODEL = "bhadresh-savani/distilbert-base-uncased-sentiment-sst2"
+# MODEL = "dhpollack/distilbert-dummy-sentiment"
 
 
+# load the model and tokenizer for the MODEL selected
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
+# save the model into bentoml's model store
 bentoml.transformers.save("sentiment_clf", model=model, tokenizer=tokenizer)
